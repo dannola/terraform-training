ec371d18df232eb335425deee6dd472be25cbf63
HashiCorp Training <training@hashicorp.com>
2017-09-18 09:30:38 +0000

    1st excercise

---
+++ training.tf
@@ -0,0 +1,30 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-958128fa
+#
+# Your subnet ID is:
+#
+#     subnet-6987b713
+#
+# Your security group ID is:
+#
+#     sg-73a74919
+#
+# Your Identity is:
+#
+#     terraform-training-falcon
+#
+
+resource "aws_instance" "web" {
+  ami                    = "ami-958128fa"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-6987b713"
+  vpc_security_group_ids = ["sg-73a74919"]
+
+  tags {
+    "Identity" = "terraform-training-falcon"
+  }
+}
