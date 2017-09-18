b39606cee26e63c123d8c0d2ea83e66d18c9174a
HashiCorp Training <training@hashicorp.com>
2017-09-18 10:40:23 +0000

    remove secrets from main.tf and add output

---
+++ main.tf
@@ -0,0 +1,38 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-958128fa
+#
+# Your subnet ID is:
+#
+#     subnet-6987b713
+#
+# Your security group ID is:
+#
+#     sg-73a74919
+#
+# Your Identity is:
+#
+#     terraform-training-falcon
+#
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "eu-central-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
